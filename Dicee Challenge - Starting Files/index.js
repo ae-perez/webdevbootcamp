3f53873f207cd19cc99064d9b7cd318241f1bc9f
Alejandra <perezalejandra.e@gmail.com>
2021-11-24 23:30:15 -0500

    finished dice game

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,47 @@
+var num1 = Math.floor(Math.random() * 6) + 1;
+var num2 = Math.floor(Math.random() * 6) + 1;
+
+// once the header is clicked sessionStorage will be set to reloading
+//this occurs first because of when you click the header
+function reloadPage() {
+    //changes the values for sessionStorage and sets to true
+    //if you didn't set these values variable reloading would always be false
+    sessionStorage.setItem("reloading", "true");
+    document.location.reload(); //then reloads the page
+}
+
+//this occurs after page is reloaded from location.reload
+window.onload = function () {
+    //store sessionStorage info in variable
+    var reloading = sessionStorage.getItem("reloading"); 
+
+    //this is going to be true
+    if (reloading) {
+        //this is done so if you actually press refresh, everything go back to default values
+        sessionStorage.removeItem("reloading"); 
+
+        myFunction();
+    }
+}
+
+function myFunction() {
+    //save the number and convert it and change first img
+    document.querySelector(".img1").setAttribute('src', `images/dice${num1}.png`);
+    document.querySelector(".img2").setAttribute('src', `images/dice${num2}.png`);
+
+    //change the top message
+    if (num1 > num2) {
+        //player 1 wins
+        document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+    } else if (num1 < num2) {
+        //player 2 wins
+        document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+
+    } else {
+        //draw!
+        document.querySelector("h1").innerHTML = "Draw!";
+
+    }
+}
+
+
