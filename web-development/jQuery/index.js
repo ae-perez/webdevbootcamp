25272a3d6bcab3071f2b8c0cfcbc64c98b714e6e
Alejandra <perezalejandra.e@gmail.com>
2021-12-08 13:58:34 -0500

    nov 8 updates

--- web-development/jQuery/index.js
+++ web-development/jQuery/index.js
@@ -1,3 +1,10 @@
 //alert("The js file is linked!");
-$("h1").css('color', '#34A87C');
+//$("h1").css('color', '#34A87C');
 
+$("h1").addClass("big-title margin-50");
+
+$("h1").text("Hi Alejandra");
+
+$("button").text("Don't Click Me");
+
+$("a").attr("href", "https://www.yahoo.com");
