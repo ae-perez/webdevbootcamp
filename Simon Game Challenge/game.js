8b32f883e839e04f68d8d0a2caf63672b6e65e2f
Alejandra <perezalejandra.e@gmail.com>
2021-12-09 03:20:29 -0500

    working on simon game

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,17 +1,86 @@
+//global variables
 const buttonColours = ["red", "blue", "green", "yellow"];
-var gamePattern= [];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
 
+
+//MAIN FUNCTION
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 3);
-
+    //get random index number
+    var randomNumber = Math.floor(Math.random() * 4);
+    //use index to get color
     var randomChosenColour = buttonColours[randomNumber];
+    //push this randomcolor to pattern array
     gamePattern.push(randomChosenColour);
-    console.log(gamePattern);
 
+    //change the title to indicate level
+    $("#level-title").text("Level " + level);
+    level++;
+
+    //play the sound of random chosen color
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    var sound = new Audio('sounds/'+ randomChosenColour +'.mp3');
-    sound.play();
+    playSound(randomChosenColour);
+
+    $(".btn").on("click", function () {
+        var userChosenColour = $(this).attr("id");
+        animatePress(userChosenColour);
+
+        userClickedPattern.push(userChosenColour);
+        playSound(userChosenColour);
+
+    });
+
 
 }
 
-nextSequence();
+
+
+
+
+//PLAY SOUND FUNCTION
+function playSound(name) {
+    //var sound = new Audio('sounds/'+ name +'.mp3');
+    //sound.play();
+
+    switch (name) {
+        case "blue":
+            var blueSound = new Audio('sounds/blue.mp3');
+            blueSound.play();
+            break;
+        case "green":
+            var greenSound = new Audio('sounds/green.mp3');
+            greenSound.play();
+            break;
+        case "red":
+            var redSound = new Audio('sounds/red.mp3');
+            redSound.play();
+            break;
+        case "yellow":
+            var yellowSound = new Audio('sounds/yellow.mp3');
+            yellowSound.play();
+            break;
+        default:
+            var wrongSound = new Audio('sounds/wrong.mp3');
+            wrongSound.play();
+            break;
+    }
+}
+
+//ADDING ANIMATION TO BUTTON FUNCTION
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+
+}
+
+$("body").keypress(function () {
+    if (!started) {
+        started = true;
+        nextSequence();
+    }
+});
